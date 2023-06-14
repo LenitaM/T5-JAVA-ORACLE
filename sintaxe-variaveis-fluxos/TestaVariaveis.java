aff06806f1455d31822780d31145a8e011ad31c4
LenitaM <lenitamaciel.ti@gmail.com>
2023-06-13 21:42:56 -0300

    tipos e variavéis = int em java

---
+++ sintaxe-variaveis-fluxos/TestaVariaveis.java
@@ -0,0 +1,30 @@
+public class TestaVariaveis {
+
+    public static void main(String[] args) {
+
+        System.out.println("Olá novo teste!"); // usado para visular variaveis, testes
+
+        /* VARIAVÉIS
+
+        Estaticamente e fortemente tipada = precisa declarar e de tipo de todas as variáveis
+        INT = Inteiros, númerico.
+        LET = Caracteres
+        DOUBLE = Ponto flutuante, numéros decimais, vírgular.
+         */
+
+        int idade;
+        idade = 26;
+
+        System.out.println(idade); // 26
+
+        //Ordem de contagem
+        idade = 4 * 5 + 6;
+
+        System.out.println(idade);
+
+        System.out.println("A sua idade é " + idade + "."); // + usado para concatenação
+
+
+    }
+
+}
