cc892c2f1ae894af32ea75564c20922376a4306e
LenitaM <lenitamaciel.ti@gmail.com>
2023-07-11 16:42:06 -0300

    Abstract no métodos e classes

--- DengoBank/ContaPoupanca.java
+++ DengoBank/ContaPoupanca.java
@@ -4,4 +4,14 @@ public class ContaPoupanca extends Conta{
     public ContaPoupanca(int agencia, int numero) {
         super(agencia, numero);
     }
+
+    @Override
+    public boolean saca(double valor) {
+        return super.saca(valor);
+    }
+
+    @Override
+    public void deposita(double valor) {
+        super.saldo += valor;
+    }
 }
