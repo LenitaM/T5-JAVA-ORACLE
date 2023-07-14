f4ec1d8bb89bc7d84652df905ff54c61cd344c13
LenitaM <lenitamaciel.ti@gmail.com>
2023-07-13 22:59:56 -0300

    CHECKED E UNCHECKED

--- java-exececoes/FluxoComTratamento.java
+++ java-exececoes/FluxoComTratamento.java
@@ -4,7 +4,7 @@ public class FluxoComTratamento {
         System.out.println("Inicio do main");
         try {
             metodo1();
-        }catch(ArithmeticException | NullPointerException ex) {
+        }catch(ArithmeticException | NullPointerException | MinhaExcecao ex) {
             String msg = ex.getMessage();
             System.out.println("Exception " + msg);
             ex.printStackTrace();
@@ -22,7 +22,7 @@ public class FluxoComTratamento {
         System.out.println("Inicio do metodo2");
 //Não basta instanciar a exceção, é necessário lançá-la através do throw.
         //tipo da variável é uma exceção
-        throw new ArithmeticException("Deu errado"); // throw = lança a bomba na pilha
+        throw new MinhaExcecao("Deu errado"); // throw = lança a bomba na pilha
         //Só funciona com exceções.
         //sai abruptamento do fluxo.
        // System.out.println("Fim do método2");
---
+++ java-exececoes/MinhaExcecao.java
@@ -0,0 +1,7 @@
+public class MinhaExcecao extends RuntimeException {
+
+    /*CRIANDO NOSSAS PRÓPRIAS EXCESSÕES*/
+    public MinhaExcecao(String msg) {
+        super(msg);
+    }
+}
